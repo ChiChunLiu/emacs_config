32a9f6cbfbeeb3efdd486bee5f85e4c5a97257ba
chichunliu <chichun.liu81@gmail.com>
2021-05-20 01:19:03 +0800

    add projectile

--- init.el
+++ init.el
@@ -1,111 +1,122 @@
-;;; emacs set-up built on Mike Zamansky's config
+;; emacs set-up tweaked from Mike Zamansky
 
-;;; Code:
 (setq inhibit-startup-message t)
 (global-linum-mode t)
+(setq linum-format "%4d")
 (tool-bar-mode -1)
 
 (setq
-   backup-by-copying t      ; don't clobber symlinks
-   backup-directory-alist
-    '(("." . "~/.saves/"))    ; don't litter my fs tree
-   delete-old-versions t
-   kept-new-versions 6
-   kept-old-versions 2
-   version-control t)       ; use versioned backups
+  backup-by-copying t      ; don't clobber symlinks
+  backup-directory-alist
+   '(("." . "~/.saves/"))    ; don't litter my fs tree
+  delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)       ; use versioned backups
 
 (require 'package)
-
-;; (add-to-list 'package-archives
-;;       '("melpa" . "http://melpa.org/packages/") t)
+(setq package-enable-at-startup nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa-stable" . "http://stable.melpa.org/packages/"))))
+ '(package-selected-packages
+   (quote
+    (ess org-vcard nord-theme ivy use-package htmlize flycheck elpy))))
 
 
 (package-initialize)
 (when (not package-archive-contents)
-  (package-refresh-contents))
+ (package-refresh-contents))
 
-(defvar ini-packages
-  '(better-defaults))
 
-(mapc #'(lambda (package)
-    (unless (package-installed-p package)
-      (package-install package)))
-       ini-packages)
+(use-package nord-theme
+  :ensure t
+  :init
+  (load-theme 'nord t)
+  )
 
-(load-theme 'nord t)
+(use-package which-key
+:ensure t
+:config
+(which-key-mode))
 
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
+;; Haskell
+(use-package haskell-mode
+  :ensure t)
+
+;; ESS
+(use-package ess
+ :ensure t
+ :init (require 'ess-site))
+
+;; org related
+(setq org-agenda-files '("~/org"))
 
 (use-package org-bullets
-:ensure t
-:config
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+ :ensure t
+ :config
+ (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
-(windmove-default-keybindings)
 
-;; it looks like counsel is a requirement for swiper
+;; Requirement for swiper
 (use-package counsel
-:ensure t
-)
+  :ensure t)
 
 (use-package swiper
-:ensure try
-:config
-(progn
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> l") 'counsel-load-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-))
+  :ensure try
+  :config
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (global-set-key "\C-s" 'swiper)
+    (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history))
+  )
 
 
 (use-package auto-complete
-:ensure t
-:init
-(progn
-(ac-config-default)
-(global-auto-complete-mode t)
-))
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t))
+  )
 
-;; pip install pylint
-(use-package flycheck
-:ensure t
-:init
-(global-flycheck-mode t))
-
-;; pip install virtualenv
-(use-package jedi
-:ensure t
-:init
-(add-hook 'python-mode-hook 'jedi:setup)
-(add-hook 'python-mode-hook 'jedi:ac-setup))
-;; M-x jedi:install-server
+(use-package elpy
+  :ensure t
+  :init
+  (elpy-enable))
 
 (use-package yasnippet
-:ensure t
-:init
-(yas-global-mode 1))
-
-(use-package yasnippet-snippets
   :ensure t
-)
+  :init
+  (yas-global-mode 1)
+  )
 
-;; (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/yasnippet-snippets/snippets")
+;; projectile
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-global-mode)
+  (setq projectile-completion-system 'ivy))
+
+(use-package counsel-projectile
+  :ensure t
+  :config
+  (counsel-projectile-mode))
 
 ;;; init.el ends here
 
-
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
